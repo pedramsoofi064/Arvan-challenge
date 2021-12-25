ecc60adb9a9b30ad898da02cffcb25c0587d4ee7
Pedram soofi <pedramsoofi064@gmail.com>
2021-12-26 00:29:49 +0330

    feat: router , vuex , prettire , eslint instaled

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  htmlWhitespaceSensitivity: 'strict',
+  vueIndentScriptAndStyle: true,
+  printWidth: 110,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+};
