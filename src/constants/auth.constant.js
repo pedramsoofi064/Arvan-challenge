3150a4aacfdd06897469c22e33fccccf6ed1f3bb
Pedram soofi <pedramsoofi064@gmail.com>
2021-12-29 19:49:49 +0330

    feat: structure

---
+++ src/constants/auth.constant.js
@@ -0,0 +1,6 @@
+export default {
+    AUTH_ROUTE_PATH:'/',
+    AUTH_ROUTE_NAME:'auth',
+    LOGIN_ROUTE_PATH: 'login',
+    LOGIN_ROUTE_NAME: 'login'
+}
--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,17 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import authMiddleware from '@/middlewares/auth.middleware'
-
+import authConstant from '@/constants/auth.constant'
 
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [
+    {
+        path: authConstant.AUTH_ROUTE_PATH,
+        name: authConstant.AUTH_ROUTE_NAME,
+        component: ()=> import('@/views/auth/auth.view.vue')
+    },
+]
 
 const router = new VueRouter({
     mode: 'history',
