1a37e39f653b44c18e000c2ca16ac84ed8b96686
Pedram soofi <pedramsoofi064@gmail.com>
2021-12-29 21:30:28 +0330

    feat: form component

---
+++ src/common/components/button.component.vue
@@ -0,0 +1,41 @@
+<template>
+  <button @click="$emit('click')" class="btn btn-block">
+        <div v-if="loading">
+            <loading color="#f9f9f9"/>
+        </div>
+        <div class="d-flex justify-center" v-else>
+            <slot />
+        </div>
+  </button>
+</template>
+
+<script>
+import Loading from './loading.component.vue'
+export default {
+    components: { Loading },
+    props: {
+        loading: {
+            type: Boolean,
+            default: false
+        },
+        size: { 
+            type: String,
+            default: 'normal'
+        }
+    },
+  
+}
+</script>
+
+<style lang="scss">
+.btn {
+    padding: 10px ;
+    border-radius: 4px;
+
+    &-primary {
+        background-color: var(--primary-color);
+        color: white;
+    }
+}
+
+</style>
--- src/views/auth/login/login.view.vue
+++ src/views/auth/login/login.view.vue
@@ -1,13 +1,29 @@
 <template>
-    <section class="auth_section login">
-        <h1 class="auth__title text-center">LOGIN</h1>
-    </section>
+  <section class="auth_section login">
+    <h1 class="auth__title text-center">LOGIN</h1>
+    <form-component auto-validation>
+      <input-component
+        v-model="model.email"
+        rules="required|email"
+        class="form__input"
+        label="Email"
+      />
+      <input-component
+        v-model="model.password"
+        rules="required"
+        type="password"
+        class="form__input"
+        label="Password"
+      />
+      <button-component :loading="loading" class="btn-primary  w-100" @click="loading = true">
+          Login
+      </button-component>
+    </form-component>
+  </section>
 </template>
 
 <script src='./login.view.js'>
-
 </script>
 
 <style lang="scss" src="./login.view.scss" scoped>
-
 </style>
