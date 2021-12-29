f4edcbcdaf1b8ce6279b70c9a5e6ae7410882627
Pedram soofi <pedramsoofi064@gmail.com>
2021-12-29 21:36:42 +0330

    feat: login

--- src/views/auth/login/login.view.vue
+++ src/views/auth/login/login.view.vue
@@ -19,6 +19,11 @@
           Login
       </button-component>
     </form-component>
+
+    <div class="auth__subtitle d-flex">
+        <span class="subtitle__text">Don’t have account?</span>
+        <router-link to="/register" class="subtitle__link fw-bold">Register Now</router-link>
+    </div>
   </section>
 </template>
 
